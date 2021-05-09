9f22547367e6f06bf314f2ff3b5437f475682795
enrreigut <enrreigut@alum.us.es>
2021-05-09 13:47:30 +0200

    Commit

--- coguard/estadisticas/views.py
+++ coguard/estadisticas/views.py
@@ -17,10 +17,16 @@ class PopulateEstadisticasTipo1(APIView):
         return Response({"msg": "Se han creado " + str(nuevas_estadisticas - prev_estadisticas) + " estadisticas" }, "200")
 
 
-
 class EstadisticasTipo1API(APIView):
     def get(self, request):
 
+        list_dias_estadisitcas = [{'fecha': x.fecha_creacion, 'provincia': x.lugar_de_residencia} for x in EstadisticasTipo1.objects.all()]
+
+        return Response(list_dias_estadisitcas, "200")
+
+
+class WebhookEstadisticasTipo1API(APIView):
+    def post(self, request):
         list_dias_estadisitcas = [{'fecha': x.fecha_creacion, 'provincia': x.lugar_de_residencia} for x in EstadisticasTipo1.objects.all()]
 
         return Response(list_dias_estadisitcas, "200")
