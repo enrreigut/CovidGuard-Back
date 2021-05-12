8985bbf753aa93291508f6ee145c9d832e334f71
enrreigut <enrreigut@alum.us.es>
2021-05-12 23:17:58 +0200

    Anadido estadisticas especficas

--- coguard/estadisticas/utils.py
+++ coguard/estadisticas/utils.py
@@ -1,9 +1,9 @@
 from .populate import *
 from django.http import JsonResponse
+import datetime
 
 
 def prettyPrint(msg):
-
     payload_data = {
         "fulfillmentMessages": [{
             "payload": {
@@ -22,17 +22,20 @@ def prettyPrint(msg):
 
 def getEstadisticasGenerales(body):
 
+    res = ""
+
     try:
         lugar_de_residencia = body['queryResult']['parameters']['LugarDeResidencia']
     except Exception as e:
-        return JsonResponse({'fulfillmentText': "Error al obtener el lugar de residencia + (" + str(e) + ")"}, safe=False)
+        return JsonResponse({'fulfillmentText': "Error al obtener el lugar de residencia + (" + str(e) + ")"},
+                            safe=False)
 
-    # Coger el dia mas reciente con estadisticas
+    # Coger fecha
 
-    fecha_mas_reciente = EstadisticasTipo1.objects.all().order_by('-fecha_creacion')[0].fecha_creacion
+    fecha = get_fecha(body)
 
     try:
-        informacion_deseada = EstadisticasTipo1.objects.get(fecha_creacion=fecha_mas_reciente,
+        informacion_deseada = EstadisticasTipo1.objects.get(fecha_creacion=fecha,
                                                             lugar_de_residencia=str(lugar_de_residencia).lower())
     except Exception as e:
         return JsonResponse({'fulfillmentText': str(e)}, safe=False)
@@ -44,6 +47,8 @@ def getEstadisticasGenerales(body):
 
 
 def getEstadisticasEspecificas(body):
+
+    res = ""
 
     try:
         lugar_de_residencia = body['queryResult']['parameters']['LugarDeResidencia']
@@ -57,17 +62,47 @@ def getEstadisticasEspecificas(body):
         return JsonResponse({'fulfillmentText': "Error al obtener el lugar de residencia + (" + str(e) + ")"},
                             safe=False)
 
-    # Coger el dia mas reciente con estadisticas
+    # Coger fecha
 
-    fecha_mas_reciente = EstadisticasTipo1.objects.all().order_by('-fecha_creacion')[0].fecha_creacion
+    fecha = get_fecha(body)
+
+    if fecha['texto']:
+        res += fecha['texto']
 
     try:
-        informacion_deseada = getattr(EstadisticasTipo1.objects.get(fecha_creacion=fecha_mas_reciente,
+        informacion_deseada = getattr(EstadisticasTipo1.objects.get(fecha_creacion=fecha['fecha'],
                                                                     lugar_de_residencia=str(
                                                                         lugar_de_residencia).lower()), columna)
     except Exception as e:
         return JsonResponse({'fulfillmentText': str(e)}, safe=False)
 
-    res = "El atributo <b>'" + columna + "'</b> para <b>" + str(lugar_de_residencia) + "</b>, es de <b>" + str(informacion_deseada) + "</b>: \n"
+
+    res += "El atributo <b>'" + columna + "'</b> para <b>" + str(lugar_de_residencia) + "</b> el <b>" + \
+          str(fecha['fecha']) + "</b>, es de <b>" + str(informacion_deseada) + "</b>\n"
 
     return res
+
+
+# Utilidades
+
+def nearest(items, pivot):
+    return min(items, key=lambda x: abs(x - pivot))
+
+
+def get_fecha(body):
+
+    texto_adicional = None
+
+    try:
+        fecha_body = datetime.datetime.strptime(body['queryResult']['parameters']['date'], '%Y-%m-%d').date()
+
+        fecha = fecha_body if len(EstadisticasTipo1.objects.filter(fecha_creacion=fecha_body)) > 0 else None
+
+        if not fecha:
+            fecha = nearest([x.fecha_creacion for x in EstadisticasTipo1.objects.all()], fecha_body)
+            texto_adicional = "No se encuentra la fecha indicada (<b>" + str(fecha_body) + "</b>). Se ha aproximado a la más cercana: <b>" + str(fecha) + "</b>\n"
+
+    except Exception as e:
+        fecha = EstadisticasTipo1.objects.all().order_by('-fecha_creacion')[0].fecha_creacion
+
+    return {'fecha': fecha, 'texto': texto_adicional}
